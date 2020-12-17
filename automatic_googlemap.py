0e03e0b3da8bc39b7bb9cb02c0150d25b84d9a70
Motoharu Ito <63852487+Motoharu-Ito-for2030@users.noreply.github.com>
2020-12-18 02:35:13 +0900

    Updated Version  everyone can use.

--- automatic_googlemap.py
+++ automatic_googlemap.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
-options = webdriver.ChromeOptions()
-options.add_argument(
-    '--user-data-dir=C:\\Users\\sketd\\AppData\\Local\\Google\\Chrome\\User Data')
 
-# この行を省略するとDefaultフォルダが指定されます
-options.add_argument('--profile-directory=Default')
+options = webdriver.ChromeOptions()
+
+options.add_argument('--user-data-dir=C:\\Users\\<ユーザ名>\\AppData\\Local\\Google\\Chrome\\User Data')
+options.add_argument('--profile-directory=Profile 2') 
 
 # driver = webdriver.Chrome(options=options)
 driver = webdriver.Chrome(
-    executable_path='C:\\Users\\sketd\\TECH.I.S\\Python\\MyPandas\\chromedriver.exe', options=options, )
+    executable_path='..\\chromedriver.exe', options=options, )
 driver.get("https://www.google.co.jp/maps/@35.7056228,139.4564374,14z?hl=ja")
 time.sleep(2)
 places = []  # list up where you want to go. 
